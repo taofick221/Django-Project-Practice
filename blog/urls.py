5913fbd7effd117f0eb9e32c77519f37006246fe
Md Taofick <107477685+taofick221@users.noreply.github.com>
2026-01-14 22:19:40 +0600

    updated myproject1

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.blog_list,name='blog_list'),
+    path('',views.home,name='home'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from datetime import datetime
 
-def blog_list(request):
-    return render(request,'blog/blog_list.html')
+class User:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    
+def home(request):
+    context={
+        "name":"Md taofick",
+        "age":23,
+        "skill":["python","java","database"],
+        "user":User("taofick",23),
+        "blog":{
+            "title":"shop",
+            "description":"<b>Welcome to our shop</b>",
+            "created_at": datetime(2025,11,12,10,12)
+        },
+        "empty_value":None,
+    }
+    return render(request,'blog/blog_list.html',context)
